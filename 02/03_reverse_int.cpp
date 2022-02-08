08e4061c500790815eb7d04e469ac2240178bf72
Shlyakhtun Prokhor <shlyakhtun.prohor@gmail.com>
2022-02-08 19:42:56 +0200

    Added task 3

---
+++ 02/03_reverse_int.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+void reverse(int &a) {
+  int reversed = 0;
+
+  while(a > 0) {
+    reversed *= 10;
+    reversed += a % 10;
+    a /= 10;
+  }
+  a = reversed;
+}
+
+
+using namespace std;
+
+int main() {
+  int a;
+  cin >> a;
+
+  reverse(a);
+
+  cout << a << endl;
+
+  return 0;
+}
